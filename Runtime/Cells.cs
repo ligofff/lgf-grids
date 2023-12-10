ce86e9a8daf2d96e646ef1758732a310ab5251d8
ligofff <liga@ligofff.ru>
2023-12-10 11:24:57 +0300

    Initial package

---
+++ Runtime/Cells.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public static class Cells
+{
+    private static Grid _grid;
+
+    public static void Initialize(GridLayout.CellLayout layout, Vector3 cellSize, Vector3 cellGap, GridLayout.CellSwizzle swizzle)
+    {
+        if (!Application.isPlaying)
+            throw new Exception("This class must be used only in play mode");
+            
+        var go = new GameObject("Static_TechnicalGrid");
+            
+        Object.DontDestroyOnLoad(go);
+        go.hideFlags = HideFlags.HideAndDontSave;
+            
+        var grid = go.AddComponent<Grid>();
+
+        grid.cellLayout = layout;
+        grid.cellSize = cellSize;
+        grid.cellGap = cellGap;
+        grid.cellSwizzle = swizzle;
+
+        _grid = grid;
+    }
+
+    public static Vector3Int WorldPosToCells(this Vector2 pos)
+    {
+        return _grid.WorldToCell(pos);
+    }
+
+    public static Vector2 CellToWorld_CentredCell(this Vector3Int cell)
+    {
+        return _grid.GetCellCenterWorld(cell);
+    }
+}
